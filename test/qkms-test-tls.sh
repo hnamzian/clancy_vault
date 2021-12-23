4f1de23ed8783615aaf912920fa1c0ea9ca2ac54
hnamzian <namazian87@gmail.com>
2021-12-23 14:36:59 +0300

    Create bash script to test tls enabled QKMS service

---
+++ test/qkms-test-tls.sh
@@ -0,0 +1,63 @@
+BASE_DIR=$HOME/workspace/Convergence/clancy
+ENV_DIR=$BASE_DIR/test/.env
+QKMS_CA_CERT=$BASE_DIR/certs/qkms_client/ca.crt
+QKMS_CLIENT_CERT=$BASE_DIR/certs/qkms_client/tls.crt
+QKMS_CLIENT_KEY=$BASE_DIR/certs/qkms_client/tls.key
+
+load_dotenv() {
+  if [ -f $ENV_DIR ]; then
+    export $(cat $ENV_DIR | sed 's/#.*//g' | xargs)
+  fi
+}
+
+list_accounts() {
+  curl -k --cacert $QKMS_CA_CERT --request GET \
+    --cert $QKMS_CLIENT_CERT --key $QKMS_CLIENT_KEY \
+    $QKMS_ADDR/stores/eth-accounts/ethereum
+}
+
+import_account() {
+  PRIV_KEY=$1
+  KEY_ID=$2
+
+  curl -k --cacert $QKMS_CA_CERT --request POST \
+    --cert $QKMS_CLIENT_CERT --key $QKMS_CLIENT_KEY \
+    -d '{ "keyId": '$KEY_ID', "privateKey": '$PRIV_KEY', "tags": { "property1": "string", "property2": "string" } }' \
+    $QKMS_ADDR/stores/eth-accounts/ethereum/import
+}
+
+send_eth_transaction() {
+  curl -k --cacert $QKMS_CA_CERT --request POST \
+    --cert $QKMS_CLIENT_CERT --key $QKMS_CLIENT_KEY \
+    $QKMS_ADDR/nodes/mumbai \
+    -d '{
+      "jsonrpc":"2.0",
+      "method":"eth_sendTransaction",
+      "params":[
+        {
+          "from": "'$WALLET_ADDR'",
+          "to": "0xbAD64d41438D5E72c11923E842C5618bAa86e73E",
+          "data": "0x6057361d0000000000000000000000000000000000000000000000000000000000000001",
+          "gasPrice": "0x1944883241",
+          "value": "0x0"
+          
+        }
+      ],
+      "id":1
+    }'
+}
+
+echo [TEST] "Load Environment Variables"
+load_dotenv
+
+echo [TEST] "List Initial Accounts"
+list_accounts
+
+echo [TEST] "Import Private Key"
+import_account $PRIVATE_KEY $KEY_ID
+
+echo [TEST] "List Accounts after imports"
+list_accounts
+
+echo [TEST] "Send Transaction to Mumbai network"
+send_eth_transaction
